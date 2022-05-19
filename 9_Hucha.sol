b929013c6ea531c27e6ff5a9508576a6a7b39af6
Fernando Lopez <cainuriel@gmail.com>
2022-05-19 11:36:03 +0200

    hucha update

--- 9_Hucha.sol
+++ 9_Hucha.sol
@@ -2,13 +2,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-//import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Hucha {
-    
-    address owner;
-    mapping(address => bool) debtor;
-    mapping(uint => uint) check; 
+contract Hucha is Ownable 
+{
+
+    mapping(address => bool) public debtor;
+    mapping(uint => uint) public check; 
 
     /**
    * Event for operations
@@ -16,50 +16,46 @@ contract Hucha {
    * @param amount of operation
    */
     event operations(string message, uint amount);
+        
+    constructor()  {}
     
-    
-    
-    modifier segurity {
-        require(msg.sender == owner);
-        _;
-    }
-    
-    constructor()  {
-        owner = msg.sender; 
-    }
-    
-    
-    function getbalance() view public returns(uint) {
+    function getbalance() view public returns(uint) 
+    {
         return address(this).balance;
     }
-    
-    function enterFunds(uint amount) public payable {
-        require(msg.value == amount);
-        emit operations("Ingreso realizado de: ", amount);
 
-              
+    
+    function enterFunds() public payable 
+    {
+ 
+        emit operations("Ingreso realizado de: ", msg.value);
+
     }
     
-     function extractFunds(uint amount) segurity public {
+     function extractFunds(uint amount) onlyOwner public 
+    {
               payable(msg.sender).transfer(amount);
               emit operations("Se han sacado: ", amount);
               
     }
        
 
-    function paycheck(uint _check) public {
+    function paycheck(uint _check) public 
+    {
             require(debtor[msg.sender], "Cheque pagado o inexistente");
             require(check[_check] != 0, "Este cheque no existe"); // si no existe el cheque devolvera cero. 
             payable(msg.sender).transfer(check[_check]);
             debtor[msg.sender] = false;
-            
-        }
+            emit operations("Se ha pagado: ", check[_check]);
+    }
         
     
-    function setdebtor(address _address, uint _checkId, uint256  _amount) segurity public {
+    function setdebtor(address _address, uint _checkId, uint256  _amount) onlyOwner public 
+    {
         check[_checkId] = _amount;
         debtor[_address] = true;
-        
+         emit operations("Se ha generado una deuda de: ", check[_check]);
     }
-    
+
+  
 }
