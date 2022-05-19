5b8f3f2469d0d92f0286c268ebcba8ea381e1d1a
Fernando Lopez <cainuriel@gmail.com>
2022-05-19 10:45:01 +0200

    ordenar contratos

---
+++ 8_Funciones_globales_del_contrato.sol
@@ -0,0 +1,49 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.4.4 <0.7.0;
+
+contract funciones_globales
+{
+    
+    //Funcion msg.sender
+    function MsgSender() public view returns(address)
+    {
+        return msg.sender;
+    }
+    
+    //funcion now
+    function Now() public view returns(uint)
+    {
+        return now; // deprecated a partir de la version 7
+    }
+    
+    //funcion block.coinbase
+    function BlockCoinbase() public view returns(address)
+    {
+        return block.coinbase; // la direccion del minero.
+    }
+    
+    //funcion block.difficulty
+    function BlockDifficulty() public view returns(uint)
+    {
+        return block.difficulty;
+    }
+    
+    //funcion block.number 
+    function BlockNumber() public view returns(uint)
+    {
+        return block.number;
+    }
+    
+    //Funcion msg.sig 
+    function MsgSig() public pure returns(bytes4)
+    {
+        return msg.sig;
+    }
+    
+    //funcion tx.gasprice
+    function txGasPrice() public view returns(uint)
+    {
+        return tx.gasprice;  // en Gweis
+    }
+    
+}
